db6f9102f1c3dcbfcf2580fb37438ca92031090a
sq9rdw <Chenyi_Ji@saic-gm.com>
2020-05-20 10:32:29 +0800

    提交作业

--- src/main/java/com/jkxy/car/api/controller/CarController.java
+++ src/main/java/com/jkxy/car/api/controller/CarController.java
@@ -6,6 +6,7 @@ import com.jkxy.car.api.utils.JSONResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -85,4 +86,27 @@ public class CarController {
         carService.insertCar(car);
         return JSONResult.ok();
     }
+    
+  //购买车辆
+    @GetMapping("buyCar/{carName}/{carType}/{carSeries}/{count}")
+    public JSONResult buyCar(@PathVariable String carName,@PathVariable String carType,@PathVariable String carSeries,@PathVariable int count) {
+         Boolean result = carService.buyCar(carName,carType,carSeries,count);
+         if (result){
+            return JSONResult.ok();
+        }else {
+            return JSONResult.errorMsg("车库数量不足");
+        }
+    }
+
+    //模糊查询分页
+    @GetMapping("findByCarNames/{carName}/{begin}/{end}")
+    public JSONResult findByCarName(@PathVariable String carName,@PathVariable int begin,@PathVariable int end) {
+        List<Car> cars = carService.findByCarName(carName);
+        List<Car> cars1 = new ArrayList<>();
+        for (int i = 0; i <cars.size() ; i++) {
+            if (i>=(begin-1) && i<=(end-1))
+                cars1.add(cars.get(i));
+        }
+        return JSONResult.ok(cars1);
+    } 
 }
